6a9f4b33158317ad19f05f983403d77d231f88d7
hayashidalex <mamihayashida@gmail.com>
2024-01-26 15:58:12 -0500

    Basic callbacks implemented

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import pandas as pd
 import plotly.graph_objects as go # or plotly.express as px
 import dash_bootstrap_components as dbc
-from dash import Dash, dcc, html
+from dash import Dash, dcc, html, Input, Output, State, callback
+import plotly.express as px 
 
 # Still hard-coded 
-# Will receive this from the slice (somehow)
+# TODO: Will receive this from the slice (somehow)
 
 node_names = ['node0', 'node1', 'node2', 'node3', 'node4', 'node5', 'node6', 'node7', 'node8', 'node9', 'node10', 'node11', 'node12', 'node13']
 
@@ -12,7 +13,7 @@ ips = ['10.129.129.2', '10.131.1.2', '10.136.129.2', '10.143.2.2', '10.134.129.2
 
 sites = ['STAR', 'MICH', 'GATECH', 'CERN', 'UCSD', 'PSC', 'HAWI', 'MASS', 'CLEM', 'AMST', 'NCSA', 'FIU', 'PRIN', 'RUTG']
 
-# Create a Dataframe with all relevant information
+# Create one Dataframe with all the geo-location information
 
 locs_df = pd.read_csv('sites.csv')
 sites_df = pd.DataFrame(sites, columns=['site'])
@@ -21,11 +22,17 @@ sites_df.insert(0, 'node_name', node_names)
 sites_df.insert(1, 'ip_address', ips)
 
 
-# Create map
+# Create another Dataframe of latency cada
+# For some reason there is an unwanted column, so delete that
+latency_df = pd.read_csv("sample.csv", header=0, comment="#")
+latency_df.drop('Unnamed: 0', axis=1, inplace=True)
 
-fig = go.Figure()
 
-fig.add_trace(go.Scattergeo(
+###############  Create map  ###########
+
+map_fig = go.Figure()
+
+map_fig.add_trace(go.Scattergeo(
         lon = sites_df['lon'],
         lat = sites_df['lat'],
         text = sites_df['site'] + '; ' + sites_df['ip_address'],
@@ -43,26 +50,25 @@ fig.add_trace(go.Scattergeo(
 #    )
 #)
 
-fig.update_geos(
+map_fig.update_geos(
     #center=dict(lon=0, lat=-80),
     lataxis_range=[-30,80],
     lonaxis_range=[-250,50]
     )
 
-fig.update_layout(
+map_fig.update_layout(
         title = 'Sites',
         #geo_scope='world',
     )
 
-# Callback + line graph
-#TODO
+# TODO: Either add a line or change the color of the src and dst
 
 
-
-
-# Layout
+######## Layout
 
 app = Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
+
+# TODO: Move the style to CSS
 
 controls = dbc.Card(
     [
@@ -72,7 +78,8 @@ controls = dbc.Card(
                 dbc.Select(
                     id="src-node",
                     options=[
-                        {"label": i, "value": i} for i in sites_df['site'].tolist()
+                        {"label": i, "value": j} \
+                         for i, j in zip(sites_df['site'].tolist(), sites_df['ip_address'].tolist())
                     ],
                     value="10.0.0.1",
                 ),
@@ -88,7 +95,8 @@ controls = dbc.Card(
                 dbc.Select(
                     id="dst-node",
                     options=[
-                        {"label": i, "value": i} for i in sites_df['site'].tolist() 
+                        {"label": i, "value": j} \
+                         for i, j in zip(sites_df['site'].tolist(), sites_df['ip_address'].tolist())
                     ],
                     value="10.0.1.1",
                 ),
@@ -100,7 +108,7 @@ controls = dbc.Card(
         ),
         html.Div(
             [
-                dbc.Label("Duration in Minutes"),
+                dbc.Label("Duration (not yet implemented)"),
                 dbc.Input(id="min", type="number", value=10),
             ],
             style={'marginBottom': 2, 
@@ -110,7 +118,7 @@ controls = dbc.Card(
         ),
         html.Div(
             [
-                dbc.Button("Submit", outline=True, color="primary"),
+                dbc.Button("Submit", id='submit-button-state', n_clicks=0, outline=True, color="primary"),
             ], 
             style={'marginBottom': 25, 
                     'marginTop': 20, 
@@ -127,14 +135,33 @@ app.layout = dbc.Container(
         html.Hr(),
         dbc.Row(
             [
-                dbc.Col(dcc.Graph(figure=fig), lg=8),
+                dbc.Col(dcc.Graph(figure=map_fig), lg=8),
                 dbc.Col(controls, lg=4),
             ],
             align="center",
         ),
+        dbc.Row(dcc.Graph(id='single-latency'))
     ],
     fluid=True,
 )
+
+########## Callbacks #########
+
+@callback(
+    Output('single-latency', 'figure'),
+    Input('submit-button-state', 'n_clicks'),
+    State('src-node', 'value'),
+    State('dst-node', 'value')
+    )
+def update_figure(n, src, dst):
+    selected_df = latency_df[latency_df['receiver'].str.contains(dst) & \
+                         latency_df['sender'].str.contains(src)]
+
+    line_fig = px.line(selected_df, x="received", y="latency",
+                title=f'{src} --> {dst} Latency (TODO: change to site names)')
+
+    return line_fig
+
 
 
 if __name__ == "__main__":
