67593510d6ea6b0d07175704c906cc611cceed29
hayashidalex <mamihayashida@gmail.com>
2024-01-26 18:40:24 -0500

    All input data are read from csv files. No more hard-coded inputs

--- app.py
+++ app.py
@@ -4,67 +4,37 @@ import dash_bootstrap_components as dbc
 from dash import Dash, dcc, html, Input, Output, State, callback
 import plotly.express as px 
 
-# Still hard-coded 
-# TODO: Will receive this from the slice (somehow)
+'''
+Requirements
+-----------
+sites.csv: site, lon, lat (for all sites)
+slice.csv: site, ip_address, node_name
+sample.csv: (downloaded from InfluxDB) latency, received, receiver, sender, seq_n, DateTime
 
-node_names = ['node0', 'node1', 'node2', 'node3', 'node4', 'node5', 'node6', 'node7', 'node8', 'node9', 'node10', 'node11', 'node12', 'node13']
+'''
 
-ips = ['10.129.129.2', '10.131.1.2', '10.136.129.2', '10.143.2.2', '10.134.129.2', '10.140.132.2', '10.147.1.2', '10.131.129.2', '10.136.1.2', '10.145.3.2', '10.132.133.2', '10.135.130.2', '10.139.130.2', '10.141.1.2']
-
-sites = ['STAR', 'MICH', 'GATECH', 'CERN', 'UCSD', 'PSC', 'HAWI', 'MASS', 'CLEM', 'AMST', 'NCSA', 'FIU', 'PRIN', 'RUTG']
+############ Input data ##############
 
 # Create one Dataframe with all the geo-location information
+# TODO This should probably be a separate function or module
 
 locs_df = pd.read_csv('sites.csv')
-sites_df = pd.DataFrame(sites, columns=['site'])
+sites_df = pd.read_csv('slice.csv')
 sites_df = sites_df.merge(locs_df)
-sites_df.insert(0, 'node_name', node_names)
-sites_df.insert(1, 'ip_address', ips)
 
 
 # Create another Dataframe of latency cada
-# For some reason there is an unwanted column, so delete that
 latency_df = pd.read_csv("sample.csv", header=0, comment="#")
+
+# For some reason there is an unwanted column, so delete that
 latency_df.drop('Unnamed: 0', axis=1, inplace=True)
 
-
-###############  Create map  ###########
-
-map_fig = go.Figure()
-
-map_fig.add_trace(go.Scattergeo(
-        lon = sites_df['lon'],
-        lat = sites_df['lat'],
-        text = sites_df['site'] + '; ' + sites_df['ip_address'],
-        marker=dict(size=8, color="blue")
-        ))
-
-#fig.add_trace(
-#    go.Scattergeo(
-#        lon = sites_df['lon'],
-#        lat = sites_df['lat'],
-#        mode = 'lines',
-#        line = dict(width = 2,color = 'red'),
-#        #hovertext=['1-2'],
-#        #textposition='top center',
-#    )
-#)
-
-map_fig.update_geos(
-    #center=dict(lon=0, lat=-80),
-    lataxis_range=[-30,80],
-    lonaxis_range=[-250,50]
-    )
-
-map_fig.update_layout(
-        title = 'Sites',
-        #geo_scope='world',
-    )
-
-# TODO: Either add a line or change the color of the src and dst
+# Convert Unix epoch time to datetime64 type
+latency_df['received'] = pd.to_datetime(latency_df['received'], unit='s')
 
 
-######## Layout
+
+############   Layout  #################
 
 app = Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 
@@ -78,10 +48,9 @@ controls = dbc.Card(
                 dbc.Select(
                     id="src-node",
                     options=[
-                        {"label": i, "value": j} \
-                         for i, j in zip(sites_df['site'].tolist(), sites_df['ip_address'].tolist())
+                        {"label": i, "value": i} for i in sites_df['site'].tolist()
                     ],
-                    value="10.0.0.1",
+                    value="STAR",
                 ),
             ],
             style={'marginBottom': 2, 
@@ -95,10 +64,9 @@ controls = dbc.Card(
                 dbc.Select(
                     id="dst-node",
                     options=[
-                        {"label": i, "value": j} \
-                         for i, j in zip(sites_df['site'].tolist(), sites_df['ip_address'].tolist())
+                        {"label": i, "value": i} for i in sites_df['site'].tolist()
                     ],
-                    value="10.0.1.1",
+                    value="STAR",
                 ),
             ],
             style={'marginBottom': 2, 
@@ -135,7 +103,7 @@ app.layout = dbc.Container(
         html.Hr(),
         dbc.Row(
             [
-                dbc.Col(dcc.Graph(figure=map_fig), lg=8),
+                dbc.Col(dcc.Graph(id='map-fig'), lg=8),
                 dbc.Col(controls, lg=4),
             ],
             align="center",
@@ -145,22 +113,72 @@ app.layout = dbc.Container(
     fluid=True,
 )
 
+
 ########## Callbacks #########
 
 @callback(
     Output('single-latency', 'figure'),
+    Output('map-fig', 'figure'),
     Input('submit-button-state', 'n_clicks'),
     State('src-node', 'value'),
     State('dst-node', 'value')
     )
 def update_figure(n, src, dst):
-    selected_df = latency_df[latency_df['receiver'].str.contains(dst) & \
-                         latency_df['sender'].str.contains(src)]
+    '''
+    Returns two graph figures
+    '''
+
+    #### Line graph  #####
+    src_ip = sites_df.loc[sites_df['site'].str.contains(src), 'ip_address'].item()
+    dst_ip = sites_df.loc[sites_df['site'].str.contains(dst), 'ip_address'].item()
+
+    #print(f'source {src_ip}, destination {dst_ip}')
+
+    selected_df = latency_df[latency_df['receiver'].str.contains(dst_ip) & \
+                         latency_df['sender'].str.contains(src_ip)]
 
     line_fig = px.line(selected_df, x="received", y="latency",
-                title=f'{src} --> {dst} Latency (TODO: change to site names)')
+                title=f'{src} ({src_ip}) --> {dst} ({dst_ip}) Latency in milliseconds')
 
-    return line_fig
+
+    #####  Map graph ######
+    map_fig = go.Figure()
+
+    map_fig.add_trace(go.Scattergeo(
+            lon = sites_df['lon'],
+            lat = sites_df['lat'],
+            text = sites_df['site'] + '; ' + sites_df['ip_address'],
+            marker=dict(size=8, color="blue")
+            ))
+   
+    
+    if n > 0:
+        # Add a line between src and dst
+
+        src_lon = sites_df.loc[sites_df['site'].str.contains(src), 'lon'].item()
+        src_lat = sites_df.loc[sites_df['site'].str.contains(src), 'lat'].item()
+        dst_lon = sites_df.loc[sites_df['site'].str.contains(dst), 'lon'].item()
+        dst_lat = sites_df.loc[sites_df['site'].str.contains(dst), 'lat'].item()
+        
+        map_fig.add_trace(
+            go.Scattergeo(
+                lon = [src_lon, dst_lon],
+                lat = [src_lat, dst_lat],
+                mode = 'lines',
+                line = dict(width = 2,color = 'red'),
+            )
+        )
+    
+    map_fig.update_geos(
+        lataxis_range=[-30,80],
+        lonaxis_range=[-250,50]
+        )
+    
+    map_fig.update_layout(
+            title = 'FABRIC slice sites',
+        )
+    
+    return line_fig, map_fig
 
 
 
